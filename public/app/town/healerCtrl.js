e07fd3345496fb26e95d853c1d4edc43060a770c
ivanholl <ivan.jelev@abv.bg>
2015-12-12 21:42:54 +0200

    Healer and Library added

---
+++ public/app/town/healerCtrl.js
@@ -0,0 +1,41 @@
+app.controller('healerCtrl', function($scope, identity, updateHero){
+    $scope.hero = identity.currentUser.heroList[0];
+    $scope.fullHealCost = getHealCost($scope.hero.maxHp - $scope.hero.hp);
+
+    if ($scope.hero.hp <= 0) {
+        $scope.hero.hp = 1;
+        updateHero.update($scope.hero, identity.currentUser);
+    }
+
+    $scope.fullHeal = function(){
+        if ($scope.hero.gold >= $scope.fullHealCost) {
+
+            $scope.hero.gold -= $scope.fullHealCost;
+            $scope.hero.hp = $scope.hero.maxHp;
+
+            updateHero.update($scope.hero, identity.currentUser);
+        }
+        else{
+            alert('Not enough gold!!!')
+        }
+    };
+
+    $scope.heal = function(healAmount){
+        var healCost = getHealCost(healAmount);
+
+        if ($scope.hero.gold >= healCost) {
+
+            $scope.hero.hp += healAmount * 1;
+            $scope.hero.gold -= healCost;
+
+            updateHero.update($scope.hero, identity.currentUser);
+        }
+        else{
+            alert('Not enough gold!!!')
+        }
+    };
+    
+    function getHealCost(amount){
+        return amount * 4;
+    }
+});
---
+++ public/app/town/libraryCtrl.js
@@ -0,0 +1,61 @@
+app.controller('libraryCtrl', function($scope, identity, updateHero){
+    var oneHpPrice = 50,
+        oneDmMinPrice = 75,
+        oneDmMaxPrice = 75,
+        oneDfPrice = 110;
+
+    $scope.hero = identity.currentUser.heroList[0];
+    
+    $scope.buyOneHp = function(){
+        if ($scope.hero.ss >= oneHpPrice) {
+
+            $scope.hero.maxHp++;
+            $scope.hero.hp++;
+            $scope.hero.ss -= oneHpPrice;
+
+            updateHero.update($scope.hero, identity.currentUser);
+        } else {
+            alert('Not Enough Soul Stones!')
+        }
+    };
+
+    $scope.buyOneDmMin = function(){
+        if ($scope.hero.ss >= oneDmMinPrice) {
+            if ($scope.hero.dm[0] < $scope.hero.dm[1]) {
+                $scope.hero.dm[0]++;
+                $scope.hero.ss -= oneDmMinPrice;
+
+                updateHero.update($scope.hero, identity.currentUser);
+            } else {
+                alert('You cannot have more minimal Dm than maximal DM')
+            }
+
+        } else {
+            alert('Not Enough Soul Stones!')
+        }
+    };
+
+    $scope.buyOneDmMax = function(){
+        if ($scope.hero.ss >= oneDmMaxPrice) {
+
+            $scope.hero.dm[1]++;
+            $scope.hero.ss -= oneDmMaxPrice;
+
+            updateHero.update($scope.hero, identity.currentUser);
+        } else {
+            alert('Not Enough Soul Stones!')
+        }
+    };
+
+    $scope.buyOneDf = function(){
+        if ($scope.hero.ss >= oneDfPrice) {
+
+            $scope.hero.df++;
+            $scope.hero.ss -= oneDfPrice;
+
+            updateHero.update($scope.hero, identity.currentUser);
+        } else {
+            alert('Not Enough Soul Stones!')
+        }
+    }
+});
