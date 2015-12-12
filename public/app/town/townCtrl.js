2587b829e1da0bde944f4ca02624e9852dd424a1
ivanholl <ivan.jelev@abv.bg>
2015-12-12 21:09:24 +0200

    Town

--- public/app/town/townCtrl.js
+++ public/app/town/townCtrl.js
@@ -1,8 +1,24 @@
 app.controller('townCtrl', function($scope, identity) {
     $scope.hero = identity.currentUser.heroList[0];
+    $scope.healer = false;
+    $scope.library = false;
 
     $scope.toMap = function () {
         $scope.hero.location = 'hzone1';
         window.location.href  = '#/map';
+    };
+
+    $scope.enterBuilding = function(building) {
+        switch (building){
+            case "library":
+                $scope.library = true;
+                $scope.healer = false;
+                break;
+            case "healer":
+                $scope.library = false;
+                $scope.healer = true;
+                break;
+            default: break;
+        }
     }
 });
