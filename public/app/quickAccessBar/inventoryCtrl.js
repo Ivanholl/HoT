e73dba969e23e66fa2d232c5b2f4f811ce3feac4
Ivan Jelev <ivan.jelev@abv.bg>
2015-12-14 16:19:12 +0000

    inventory

--- public/app/quickAccessBar/inventoryCtrl.js
+++ public/app/quickAccessBar/inventoryCtrl.js
@@ -1,5 +1,13 @@
 app.controller('inventoryCtrl', function($scope, identity){
     $scope.hero = identity.currentUser.heroList[0];
     $scope.inventory = $scope.hero.inventory;
-    console.log('zdr')
+    
+    if($scope.hero.inventory.length <= 0) {
+    $scope.hero.inventory.push({
+                        title:"Stick", 
+                        type:'Weapon',
+                        weight:'1', 
+                        bonus:['DMmin+1']
+                    })
+    }
 });
