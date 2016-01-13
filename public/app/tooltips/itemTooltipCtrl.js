b0e900e5b72e179bc7ddc81216c48deb42e82f07
ivanholl <ivan.jelev@abv.bg>
2016-01-13 14:48:49 +0200

    tooltip work

--- public/app/tooltips/itemTooltipCtrl.js
+++ public/app/tooltips/itemTooltipCtrl.js
@@ -1,4 +1,10 @@
 app.controller("ItemTooltipCtrl", function($scope, identity){
     $scope.hero = identity.currentUser.heroList[0];
     $scope.tooltipItem;
+    $scope.displayBonus = [];
+
+    for(i=1; i <= $scope.tooltipItem.bonus.length; i+=2){
+        var displayString = "+" + $scope.tooltipItem.bonus[i] + " " + $scope.tooltipItem.bonus[i-1];
+        $scope.displayBonus.push(displayString);
+    }
 });
