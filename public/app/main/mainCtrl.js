39029e03931c811d1f6f2cc872d4b85c8dd46b86
ivanholl <ivan.jelev@abv.bg>
2016-06-19 14:13:28 +0300

    css fixes

--- public/app/main/mainCtrl.js
+++ public/app/main/mainCtrl.js
@@ -1,4 +1,8 @@
 app.controller('MainCtrl', function($scope, identity, ngAudio) {
     $scope.identity = identity;
     $scope.sound = ngAudio.load("sounds/mouseHover.mp3");
+
+    $scope.play = function(){
+        window.location = "#/hero";
+    }
 });
